335e47d12269d0beab10f2ccb08d23cc000ba3c0
batool-alghamdi <82464731+batool-alghamdi@users.noreply.github.com>
2021-05-29 22:14:21 +0300

    Add files via upload

---
+++ PizzaPizza/PizzaPizza/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaPizza
+{
+    public interface IPizzaBuilder
+    {
+        void BuildDough();
+        void BuildSauce();
+        void BuildTopping();
+    }
+
+    public class WhitePizzaBuilder : IPizzaBuilder
+    {
+        private Pizza pizza;
+        public WhitePizzaBuilder()
+        {
+            this.Reset();
+        }
+        public void Reset()
+        {
+            this.pizza = new Pizza();
+        }
+
+        public Pizza GetPizza()
+        {
+            Pizza result = this.pizza;
+            this.Reset();
+            return result;
+        }
+
+        public void BuildDough()
+        {
+            this.pizza.Add("White Dough");
+        }
+
+        public void BuildSauce()
+        {
+            this.pizza.Add("white Sauce");
+        }
+
+        public void BuildTopping()
+        {
+            this.pizza.Add("olive");
+        }
+
+    }
+
+
+    public class WheatPizzaBuilder : IPizzaBuilder
+    {
+        private Pizza pizza;
+        public WheatPizzaBuilder()
+        {
+            this.Reset();
+        }
+        public void Reset()
+        {
+            this.pizza = new Pizza();
+        }
+
+        public Pizza GetPizza()
+        {
+            Pizza result = this.pizza;
+            this.Reset();
+            return result;
+        }
+
+        public void BuildDough()
+        {
+            this.pizza.Add("Wheat Dough");
+        }
+
+        public void BuildSauce()
+        {
+            this.pizza.Add("Tomato Sauce");
+        }
+
+        public void BuildTopping()
+        {
+            this.pizza.Add("Cheese");
+        }
+
+    }
+
+    public class Pizza
+    {
+        private List<object> ingredients = new List<object>();
+        public void Add(string part)
+        {
+            this.ingredients.Add(part);
+        }
+        public string listIngredients()
+        {
+            string str = "";
+            for (int i = 0; i < this.ingredients.Count; i++)
+            {
+                if (i < this.ingredients.Count - 1) str += this.ingredients[i] + ",";
+                else str += this.ingredients[i] + ".";
+            }
+            return "Pizaa ingredients are:" + str;
+        }
+    }
+
+
+    public class Director
+    {
+        private IPizzaBuilder builder;
+        public IPizzaBuilder Builder
+        {
+            set { this.builder = value; }
+        }
+        public void WithSuase()
+        {
+            this.builder.BuildDough();
+            this.builder.BuildSauce();
+            this.builder.BuildTopping();
+        }
+        public void WithoutSuase()
+        {
+            this.builder.BuildDough();
+            this.builder.BuildSauce();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var director = new Director();
+            //wheat
+            var wheatPizzaBuilder = new WheatPizzaBuilder();
+            director.Builder = wheatPizzaBuilder;
+
+            director.WithSuase();
+            Console.WriteLine(wheatPizzaBuilder.GetPizza().listIngredients());
+
+            director.WithoutSuase();
+            Console.WriteLine(wheatPizzaBuilder.GetPizza().listIngredients());
+            //white
+            var whitePizzaBuilder = new WhitePizzaBuilder();
+            director.Builder = whitePizzaBuilder;
+
+            director.WithSuase();
+            Console.WriteLine(whitePizzaBuilder.GetPizza().listIngredients());
+        }
+    }
+}
