971f5e02f0eed381617053349b3600b75fc4ca61
ThamerMashni <thamer.mashni@tuwaiq.edu.sa>
2021-05-29 23:50:53 +0300

    pizza builder

---
+++ PizzaBuilder/PizzaBuilder/IBuilder.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaBuilder
+{
+    public interface IPizzaBuilder
+    {
+        abstract void Reset();
+        void BuildDough();
+        void BuildSauce();
+        void BuildTopping(string topping);
+    }
+
+    public class WhitePizzaBuilder : IPizzaBuilder
+    {
+        private WhitePizza _product = new WhitePizza();
+        public WhitePizzaBuilder()
+        {
+            this.Reset();
+        }
+        public void Reset()
+        {
+            this._product = new WhitePizza();
+        }
+    
+        public WhitePizza GetPizza()
+        {
+            WhitePizza result = this._product;
+            this.Reset();
+            return result;
+        }
+
+        public void BuildDough()
+        {
+            this._product.Add("White Dough");
+        }
+
+        public void BuildSauce()
+        {
+            this._product.Add("Sauce");
+        }
+
+        public void BuildTopping(string topping)
+        {
+            this._product.Add(topping);
+        }
+
+    }
+
+    public class WheatPizzaBuilder : IPizzaBuilder
+    {
+        private WheatPizza _product = new WheatPizza();
+        public WheatPizzaBuilder()
+        {
+            this.Reset();
+        }
+        public void Reset()
+        {
+            this._product = new WheatPizza();
+        }
+
+        public WheatPizza GetPizza()
+        {
+            WheatPizza result = this._product;
+            this.Reset();
+            return result;
+        }
+
+        public void BuildDough()
+        {
+            this._product.Add("Wheat Dough");
+        }
+
+        public void BuildSauce()
+        {
+            this._product.Add("Sauce");
+        }
+
+        public void BuildTopping(string topping)
+        {
+            this._product.Add(topping);
+        }
+    }
+
+    public class WhitePizza
+    {
+        private List<object> parts = new List<object>();
+        public void Add(string part)
+        {
+            this.parts.Add(part);
+        }
+        public string listParts()
+        {
+            string str = "";
+            for (int i = 0; i < this.parts.Count; i++)
+            {
+                str += this.parts[i] + ",";
+            }
+            return "Pizza ingredients are :" + str;
+        }
+    }
+
+    public class WheatPizza
+    {
+        private List<object> parts = new List<object>();
+        public void Add(string part)
+        {
+            this.parts.Add(part);
+        }
+        public string listParts()
+        {
+            string str = "";
+            for (int i = 0; i < this.parts.Count; i++)
+            {
+                str += this.parts[i] + ",";
+            }
+            return "Pizza ingredients are :" + str;
+        }
+    }
+
+    public class Director
+    {
+        private IPizzaBuilder _builder;
+        public IPizzaBuilder Builder
+        {
+            set { this._builder = value; }
+        }
+
+        public void MakeMeatLoverPizza()
+        {
+            this._builder.BuildDough();
+            this._builder.BuildSauce();
+            this._builder.BuildTopping("Pepperoni");
+            this._builder.BuildTopping("Ground beef");
+            this._builder.BuildTopping("Cheese");
+        }
+        public void MakeMargaritaPizza()
+        {
+            this._builder.BuildDough();
+            this._builder.BuildSauce();
+            this._builder.BuildTopping("Cheese");
+        }
+    }
+
+}
---
+++ PizzaBuilder/PizzaBuilder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PizzaBuilder
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //client
+            var director = new Director();
+            var builder = new WhitePizzaBuilder();
+            director.Builder = builder;
+
+            //Meat Lover pizza 
+            director.MakeMeatLoverPizza();
+
+            Console.WriteLine(builder.GetPizza().listParts());
+            //Min Product
+            director.MakeMargaritaPizza();
+            Console.WriteLine(builder.GetPizza().listParts());
+
+            //Custom WheatPizza Product
+            var customBuilder = new WheatPizzaBuilder();
+            customBuilder.BuildDough();
+            customBuilder.BuildSauce();
+            customBuilder.BuildTopping("Chicken");
+            customBuilder.BuildTopping("Cheese");
+            Console.WriteLine(customBuilder.GetPizza().listParts());
+        }
+    }
+    
+}
