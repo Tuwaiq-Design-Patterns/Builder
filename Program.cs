c20673ab16c86f7680848b79ef6321d2a8a1bd9d
Mansour-Aldundur <mansour.aldundur@tuwaiq.edu.sa>
2021-05-29 21:44:11 +0300

    add: Program Files

---
+++ Program.cs
@@ -0,0 +1,194 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+
+namespace builder
+{
+    public interface IPizzaBuilder
+    {
+        void BuildDough(string doughType);
+        void BuildSauce(string sauceType);
+        void BuildTopping(string toppingType);
+        void Reset(); 
+    }
+    public class WhitePizzaBuilder : IPizzaBuilder
+    {
+        private WhitePizza _product = new();
+        public WhitePizzaBuilder()
+        {
+            this.Reset();
+        }
+        public void Reset()
+        {
+            this._product = new WhitePizza();
+        }
+        public void BuildDough(string doughType = null )
+        {
+            if(doughType == null || string.IsNullOrEmpty(doughType) )
+            {
+                this._product.Add("White Dough");
+            }
+            else
+            {
+                this._product.Add(doughType);
+            }
+        }
+
+        public void BuildSauce(string sauceType = null)
+        {
+            if(sauceType == null || string.IsNullOrEmpty(sauceType))
+            {
+                this._product.Add("tomato Sauce");
+            }
+            else
+            {
+                this._product.Add(sauceType);
+            }
+        }
+        public void BuildTopping(string toppingType= null)
+        {
+            if(toppingType == null || string.IsNullOrEmpty(toppingType))
+            {
+                this._product.Add("Cheese Topping");
+            }
+            else
+            {
+                this._product.Add(toppingType);
+            }
+        }
+        public WhitePizza GetPizza()
+        {
+            WhitePizza result = this._product;
+            this.Reset();
+            return result;
+        }
+    }
+    public class WhitePizza
+    {
+        private List<object> parts = new List<object>();
+        public void Add(string part)
+        {
+            this.parts.Add(part);
+        }
+        public string ListParts()
+        {
+            return "White Pizza: " + string.Join(",", this.parts);
+        }
+    }
+    
+    public class WheatPizzaBuilder : IPizzaBuilder
+    {
+        private WheatPizza _product = new();
+        public WheatPizzaBuilder()
+        {
+            this.Reset();
+        }
+        public void Reset()
+        {
+            this._product = new WheatPizza();
+        }
+        public void BuildDough(string doughType=null)
+        {
+            if(doughType == null ||string.IsNullOrEmpty(doughType) )
+            {
+                this._product.Add("Wheat Dough");
+            }
+            else
+            {
+                this._product.Add(doughType);
+            }
+        }
+        public void BuildSauce(string sauceType=null)
+        {
+            if(sauceType == null ||  string.IsNullOrEmpty(sauceType))
+            {
+                this._product.Add("Italian Sauce");
+            }
+            else
+            {
+                this._product.Add(sauceType);
+            }
+        }
+        public void BuildTopping(string toppingType=null)
+        {
+            if(toppingType == null ||  string.IsNullOrEmpty(toppingType))
+            {
+                this._product.Add("Cheese Marai Topping");
+            }
+            else
+            {
+                this._product.Add(toppingType);
+            }
+        }
+        public WheatPizza GetPizza()
+        {
+            WheatPizza result = this._product;
+            this.Reset();
+            return result;
+        }
+    }
+    public class WheatPizza
+    {
+        private List<object> parts = new List<object>();
+        public void Add(string part)
+        {
+            this.parts.Add(part);
+        }
+        public string ListParts()
+        {
+            return "Wheat Pizaa: " + string.Join(",", this.parts);
+        }
+    }
+    public class Director
+    {
+        private IPizzaBuilder _builder;
+
+        public Director(IPizzaBuilder pizzaBuilder)
+        {
+            this._builder = pizzaBuilder;
+        }
+        public void MakePizza()
+        {
+            WriteLine("Dough ? : Or tra Default");
+            this._builder.BuildDough( ReadLine());
+            WriteLine("Sauce ? : Or tra Default");
+            this._builder.BuildSauce( ReadLine());
+            WriteLine("Topping ?: Or tra Default ");
+            this._builder.BuildTopping( ReadLine());
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            //client
+            WhitePizzaBuilder whitePizzaBuilder = new();
+            Director director = new(whitePizzaBuilder);
+            // Bonus Two user prompt 
+            director.MakePizza();
+            WriteLine("------------------------------------");
+            WriteLine("Here IS your White Pizza ");
+            WriteLine("------------------------------------");
+            WriteLine(whitePizzaBuilder.GetPizza().ListParts());
+            WriteLine("------------------------------------");
+            
+            WheatPizzaBuilder wheatPizzaBuilder = new();
+            director = new(wheatPizzaBuilder);
+            director.MakePizza();
+            WriteLine("------------------------------------");
+            WriteLine("Here IS your Brown Pizza");
+            WriteLine("------------------------------------");
+            WriteLine(wheatPizzaBuilder.GetPizza().ListParts());
+            WriteLine("------------------------------------");
+            
+             // Bonus One 
+             WriteLine("====================================");
+             WriteLine("Created Without Director");
+             WriteLine("------------------------------------");
+             whitePizzaBuilder.BuildSauce("Green Paper Sauce");
+             whitePizzaBuilder.BuildTopping("Jalapeno");
+             whitePizzaBuilder.BuildDough("Honey White");
+             WriteLine(whitePizzaBuilder.GetPizza().ListParts());
+             WriteLine("------------------------------------");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Builder
 
+### عمل الطالب 
+#### منصور آل دندور 
+
+### اشراف المدربة 
+#### غادة المطيري 
+
+### فريق 
+#### السودة
+
 ## Builder هيكلة 
 
 <img width="845" alt="Screen Shot 1442-10-16 at 7 49 08 PM" src="https://user-images.githubusercontent.com/82456787/120017012-00b64900-bfee-11eb-9e65-7fc00699d8b1.png">
